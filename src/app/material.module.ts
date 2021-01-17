7540936ca434c552ef363eb3fbcfd6776b901666
Ricardo <rirova81@gmail.com>
2021-01-17 19:25:06 +0100

    Add checbox and set datepicker to required

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,6 +10,7 @@ import { MatInputModule } from '@angular/material/input';
 @NgModule({
   imports: [
     MatButtonModule,
+    MatCheckboxModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
@@ -17,6 +19,7 @@ import { MatInputModule } from '@angular/material/input';
   ],
   exports: [
     MatButtonModule,
+    MatCheckboxModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
