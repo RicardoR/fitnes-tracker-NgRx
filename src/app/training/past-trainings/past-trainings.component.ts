2db8549886dfcbd3920dfdb4a24eb8662d565c60
Ricardo <rirova81@gmail.com>
2021-02-12 20:34:49 +0100

    Fix past training exercises

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,28 +1,19 @@
-import {
-  AfterViewInit,
-  Component,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { Exercise } from '../models/exercise.model';
+import * as fromTraining from '../reducers/training.reducer';
 import { TrainingService } from './../training.service';
-
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.scss'],
 })
-export class PastTrainingsComponent
-  implements OnInit, AfterViewInit, OnDestroy {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  private _exerciseSubscription: Subscription;
 
   public dataSource = new MatTableDataSource<Exercise>();
   public displayedColumns: string[] = [
@@ -33,13 +24,17 @@ export class PastTrainingsComponent
     'state',
   ];
 
-  constructor(private _trainingService: TrainingService) {}
+  constructor(
+    private _trainingService: TrainingService,
+    private _store: Store<fromTraining.State>
+  ) {}
 
   ngOnInit(): void {
     this._trainingService.fetchCompletedOrCancellExercises();
-    // this._exerciseSubscription = this._trainingService.finishedExercisesChanged.subscribe(
-    //   (exercises: Exercise[]) => (this.dataSource.data = exercises)
-    // );
+
+    this._store
+      .select(fromTraining.getFinishedExercises)
+      .subscribe((exercises: Exercise[]) => (this.dataSource.data = exercises));
   }
 
   public doFilter(filter: string): void {
@@ -49,11 +44,5 @@ export class PastTrainingsComponent
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
-  }
-
-  ngOnDestroy(): void {
-    if (this._exerciseSubscription) {
-      this._exerciseSubscription.unsubscribe();
-    }
   }
 }
